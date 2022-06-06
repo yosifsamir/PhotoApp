88442e676d2d86ced9410909e3aee587590a7614
yosifsamir <37003043+yosifsamir@users.noreply.github.com>
2022-06-06 03:57:35 +0200

    PhotoApp

--- app/src/main/java/com/example/photoapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/photoapp/ui/gallery/GalleryFragment.kt
@@ -2,7 +2,10 @@ package com.example.photoapp.ui.gallery
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -33,6 +36,29 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
             override fun onChanged(pagingUnsplashPhoto: PagingData<UnsplashPhoto>?) {
                 adapter.submitData(viewLifecycleOwner.lifecycle, pagingUnsplashPhoto!!)
             }
+        })
+        setHasOptionsMenu(true) // show option menu in fragment
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.gallery_menu,menu)
+        val searchItem=menu.findItem(R.id.action_search) // it is MenuItem not SearchView
+        val searchView=searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query!=null){
+                    binding.galleryRecyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus() // to hide keyboard when you submit the search
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
         })
     }
 
